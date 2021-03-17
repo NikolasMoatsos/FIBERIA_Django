2bc99b0b2f0a9aedb2234b514ffb85a8270dd250
Nikolas Moatsos <nmoazzo@gmail.com>
2021-03-17 20:01:10 +0200

    'View products' use case with Django

--- Fiberia_django/settings.py
+++ Fiberia_django/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+y@l(u9w+t8my#=u#dqdb9ehldju!uv_wtn391va5us-vcc#85'
+# SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
